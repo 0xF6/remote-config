6bb123e5d4880054eab172fd88737de96dfefad7
Yuuki Wesp <ls-micro@ya.ru>
2020-12-04 03:40:29 +0300

    add service side

---
+++ src/service/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ivy
+{
+    using System;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(Configure)
+                .Build()
+                .Run();
+
+        public static void Configure(IWebHostBuilder webBuilder)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            webBuilder
+                .UseStartup<Startup>()
+                .UseUrls($"http://*.*.*.*:{port}");
+        }
+    }
+}
---
+++ src/service/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace ivy
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using middleware;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) 
+            => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IFireStoreAdapter, FireStoreAdapter>();
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddRouting(options => options.LowercaseUrls = true);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) 
+                app.UseDeveloperExceptionPage();
+            app.UseBotMiddleware();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
